104b5f889fbe2183e9f7e75cfc21e986b56d94b0
DucViet <vietduc.tran@apuscorp.com>
2023-05-29 10:55:26 +0700

    fix message error

--- src/main/java/com/kma/project/expensemanagement/config/LocaleConfiguration.java
+++ src/main/java/com/kma/project/expensemanagement/config/LocaleConfiguration.java
@@ -1,15 +1,9 @@
 package com.kma.project.expensemanagement.config;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import javax.annotation.PostConstruct;
-import java.util.Locale;
 import java.util.TimeZone;
 
 @Configuration
@@ -23,18 +17,19 @@ public class LocaleConfiguration implements WebMvcConfigurer {
         TimeZone.setDefault(TimeZone.getTimeZone("GMT+7"));
     }
 
-    @Bean
-    public MessageSource messageSource() {
-        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
+//    @Bean
+//    public MessageSource messageSource() {
+//        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+//        messageSource.setBasename("classpath:messages");
+//        messageSource.setDefaultEncoding("UTF-8");
+//        return messageSource;
+//    }
 
-    @Bean
-    public LocaleResolver localeResolver() {
-        AcceptHeaderLocaleResolver acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
-        acceptHeaderLocaleResolver.setDefaultLocale(Locale.US);
-        return acceptHeaderLocaleResolver;
-    }
+
+//    @Bean
+//    public LocaleResolver localeResolver() {
+//        AcceptHeaderLocaleResolver acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
+//        acceptHeaderLocaleResolver.setDefaultLocale(Locale.);
+//        return acceptHeaderLocaleResolver;
+//    }
 }
