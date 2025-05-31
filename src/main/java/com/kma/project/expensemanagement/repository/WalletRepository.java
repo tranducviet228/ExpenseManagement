98762450d8f4874555a5bb1f9477a947f53e821d
Duc Viet <vietduc.tran@apuscorp.com>
2025-06-01 00:45:49 +0700

    fix query api get list wallet

--- src/main/java/com/kma/project/expensemanagement/repository/WalletRepository.java
+++ src/main/java/com/kma/project/expensemanagement/repository/WalletRepository.java
@@ -12,6 +12,7 @@ public interface WalletRepository extends JpaRepository<WalletEntity, Long> {
 
     Page<WalletEntity> findAllByCreatedBy(Pageable pageable, Long createdBy);
 
+    @Query(value = " select w from WalletEntity w where (w.createdBy = :createdBy) or (:groupId is not null and w.groupId = :groupId) order by w.createdAt")
     List<WalletEntity> findAllByCreatedByOrGroupIdOrderByCreatedAt(Long createdBy, Long groupId);
 
     @Query(value = " select w.id from WalletEntity w where w.createdBy = :userId or w.groupId = :groupId")
